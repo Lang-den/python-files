993cf01d776055302de39c6047a1ed64f9d9f170
Hayden Langston <langstoh@student.psdr3.org>
2020-01-13 13:18:24 -0600

    added more

--- 2.1/a214.py
+++ 2.1/a214.py
@@ -1,15 +1,25 @@
 import tkinter
 
+
 def printtext():
-    print(name_input.get())
+    
+    if len(pass_input.get()) >= 8 and len(pass_input.get()) <= 14:
+        print('User:', user_input.get(),'\nPass:', pass_input.get())
 
 root = tkinter.Tk()
 root.geometry("500x500")
 
 user_label = tkinter.Label(root, text="Username:")
 user_label.pack()
+user_input = tkinter.Entry(root)
+user_input.pack()
 
-name_input = tkinter.Entry(root)
-name_input.pack()
+pass_label = tkinter.Label(root, text="Password:")
+pass_label.pack()
+pass_input = tkinter.Entry(root, show="")
+pass_input.pack()
+
+submit_button = tkinter.Button(root, text="Submit", command=printtext)
+submit_button.pack()
 
 root.mainloop()
